1a610a058603ad0149395d515fa68b3063dc8896
Jop Wolterink <jop.wolterink@gmail.com>
2018-09-26 12:13:05 +0200

    sdf
    asf

--- Wagenpark/Wagenpark/Controllers/MijnBoekingController.cs
+++ Wagenpark/Wagenpark/Controllers/MijnBoekingController.cs
@@ -19,8 +19,32 @@ namespace Wagenpark.Controllers
        
 
         // GET: Boeking
-        public ActionResult Index()
+        public ActionResult Index(string updaten, int? boekingid, string voornaam, string achternaam, string postcode, string woonplaats)
         {
+
+            if (updaten != null)
+            {
+                if (!updaten.Equals("True"))
+                {
+                    var boeking = from i in db.Boekingen where i.Boekingid == boekingid select i;
+                    db.Boekingen.Remove(boeking.FirstOrDefault());
+                    db.SaveChanges();
+
+                }
+                else
+                {
+                    // updaten
+                    var gebruiker = from s in db.Gasten where s.Email == User.Identity.Name select s;
+                    if (gebruiker.Any())
+                    {
+                        gebruiker.FirstOrDefault().Voornaam = voornaam;
+                        gebruiker.FirstOrDefault().Achternaam = achternaam;
+                        gebruiker.FirstOrDefault().Woonplaats = woonplaats;
+                        gebruiker.FirstOrDefault().Postcode = postcode;
+                        db.SaveChanges();
+                    }
+                }
+            }
             var gebruikerid = from a in db.Gasten where a.Email == User.Identity.Name select a.GastenID;
 
             var boekingen = from i in db.Boekingen where i.gastID == gebruikerid.FirstOrDefault() select i;
@@ -45,34 +69,41 @@ namespace Wagenpark.Controllers
             DateTime incheckdatum = model.incheckdatum;
             DateTime uitcheckdatum = model.uitcheckdatum;
 
-
-
-
-            var lodges = (from dbLoges in db.Lodges
-                         where (incheckdatum < dbLoges.Boekingen.FirstOrDefault().incheckdatum && uitcheckdatum < dbLoges.Boekingen.FirstOrDefault().incheckdatum) ||
-                         (incheckdatum > dbLoges.Boekingen.FirstOrDefault().uitcheckdatum && uitcheckdatum > dbLoges.Boekingen.FirstOrDefault().uitcheckdatum)
-                         select dbLoges).ToList();
-
-
             List<LodgeTypes> lodgeTypes = new List<LodgeTypes>();
 
-            foreach (var item in lodges)
+
+            var boekingen = from a in db.Boekingen select a;
+
+            if (!boekingen.Any())
+            {
+                lodgeTypes = (from lod in db.LodgeTypes select lod).Distinct().ToList();
+                
+
+            }
+            else
             {
 
+                var lodges = (from dbLoges in db.Lodges
+                              where (incheckdatum < dbLoges.Boekingen.FirstOrDefault().incheckdatum && uitcheckdatum < dbLoges.Boekingen.FirstOrDefault().incheckdatum) ||
+                              (incheckdatum > dbLoges.Boekingen.FirstOrDefault().uitcheckdatum && uitcheckdatum > dbLoges.Boekingen.FirstOrDefault().uitcheckdatum)
+                              select dbLoges).ToList();
 
-                lodgeTypes.Add(item.LodgeTypes);
+                foreach (var item in lodges)
+                {
+
+
+                    lodgeTypes.Add(item.LodgeTypes);
+                }
+
             }
 
             lodgeTypes.Distinct().ToList();
+
 
             BoekenModel boekmodle = new BoekenModel();
             boekmodle.lodgestypes = lodgeTypes;
             boekmodle.incheckdatum = incheckdatum;
             boekmodle.uitcheckdatum = uitcheckdatum;
-
-
-            //lodgeTypes = (from i in db.Lodges where i.Bezettingsgraad == true select i.LodgeTypes).Distinct().ToList();
-
             return View(boekmodle);
         }
 
